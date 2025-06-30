52e581b434e355a13aa0aaa8312a35a3693aabeb
Irina Migunova <ista.irina@gmail.com>
2025-06-30 21:48:20 +0300

    add package.json to develop branch

--- module-tests/quadratic-equation.test.js
+++
@@ -1,6 +0,0 @@
-const QuadraticEquation = require('./quadratic-equation.js');
-
-test('should return an empty array for x^2 + 1 = 0', () => {
-  const equation = new QuadraticEquation(1, 0, 1);
-  expect(equation.solve()).toEqual([]);
-});
