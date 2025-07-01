e5dfb3548342dc542eb08588c0e759bf3bd7bfef
Irina Migunova <ista.irina@gmail.com>
2025-07-02 01:06:30 +0300

    solid-principles: add solution and test for rotation, change class implementation, fix bugs

--- solid-principles/solid-principles.js
+++ solid-principles/solid-principles.js
@@ -1,42 +1,81 @@
-class UObject {}
-
-class MovableObject {
+// у любого объекта есть положение в пространстве, значит мы можем узнать эту позицию
+// и установаить её.
+class UObject {
   constructor() {
     this.x;
     this.y;
-    this.velocity;
-  }
-
-  setPosition({ x, y }) {
-    this.x = x;
-    this.y = y;
   }
 
   getPosition() {
     return { x: this.x, y: this.y };
   }
 
+  setPosition({ x, y }) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+// объекты, которые могут перемещаться, имеют те же свойства, что и универсальные объекты,
+// следовательно наследуемся от класса UObject, но также обладает собственным свойствами
+// и методами - скорость и установка скорости.
+
+class MovableObject extends UObject {
+  constructor() {
+    super();
+    this.velocity;
+    this.angle = 0;
+  }
+
   setVelocity({ x, y }) {
     this.velocity = { x, y };
   }
 
-  move() {
-    if (!this.x || !this.y) {
-      throw new Error('Position is not set');
-    }
-
-    if (!this.velocity) {
-      throw new Error('Velocity is not set');
-    }
-    
-    this.x += this.velocity.x;
-    this.y += this.velocity.y;
+  getAngle() {
+    return this.angle;
   }
 }
 
-class Move {}
+// каждое движение объекта - это смещение его координат: (x, y) = (x + dx, y + dy)
+// перед перемещением добавлены проверки, что объект имеет координаты, скорость
+// и является перемещаемым. 
+class Movement {
+  move(movableObject) {
+    if (!(movableObject instanceof MovableObject)) {
+      throw new Error('The object cannot move');
+    }
 
-class Rotate {}
+    if (movableObject.x === undefined || movableObject.y === undefined) {
+      throw new Error('Position is not set');
+    }
+
+    if (!movableObject.velocity) {
+      throw new Error('Velocity is not set');
+    }
+
+    movableObject.x += movableObject.velocity.x;
+    movableObject.y += movableObject.velocity.y;
+  }
+}
 
 
-module.exports = { UObject, MovableObject, Move, Rotate }
+// Вращение объекта - изменение его угла. Происходит по аналогии с перемещением
+class Rotation {
+    rotate(movableObject, angle) {
+      if (!(movableObject instanceof MovableObject)) {
+        throw new Error('The object cannot rotate');
+      }
+
+      if (angle === undefined) {
+        throw new Error('Angle is not set');
+      }
+  
+      if (movableObject.x === undefined || movableObject.y === undefined) {
+        throw new Error('Position is not set');
+      }
+  
+      movableObject.angle = (movableObject.angle + angle) % 360;
+    }
+}
+
+module.exports = { UObject, MovableObject, Movement, Rotation };
--- solid-principles/solid-principles.test.js
+++ solid-principles/solid-principles.test.js
@@ -1,23 +1,59 @@
-const SpaceObject = require('./solid-principles');
+const {
+  MovableObject,
+  UObject,
+  Movement,
+  Rotation,
+} = require('./solid-principles');
 
 describe('SpaceObjects used SOLID principles', () => {
-    it('should move', () => {
-        const spaceObject = new SpaceObject.MovableObject();
-        spaceObject.setPosition({ x: 12, y: 5 });
-        spaceObject.setVelocity({ x: -7, y: 3 });
-        spaceObject.move();
-        expect(spaceObject.getPosition()).toEqual({ x: 5, y: 8 });
-    });
+  const movement = new Movement();
+  const rotation = new Rotation();
 
-    it('throw exception if position is not set', () => {
-        const spaceObject = new SpaceObject.MovableObject();
-        spaceObject.setVelocity({ x: -7, y: 3 });
-        expect(() =>spaceObject.move()).toThrow();
-    });
+  it('should move movable object', () => {
+    const spaceObject = new MovableObject();
 
-    it('throw exception if velocity is not set', () => {
-        const spaceObject = new SpaceObject.MovableObject();
-        spaceObject.setPosition({ x: 12, y: 5 });
-        expect(() => spaceObject.move()).toThrow();
-    });
+    spaceObject.setPosition({ x: 12, y: 5 });
+    spaceObject.setVelocity({ x: -7, y: 3 });
+    movement.move(spaceObject);
+    expect(spaceObject.getPosition()).toEqual({ x: 5, y: 8 });
+  });
+
+  it('throw exception if position is not set', () => {
+    const spaceObject = new MovableObject();
+    spaceObject.setVelocity({ x: -7, y: 3 });
+    expect(() => movement.move(spaceObject)).toThrow('Position is not set');
+  });
+
+  it('throw exception if velocity is not set', () => {
+    const spaceObject = new MovableObject();
+    spaceObject.setPosition({ x: 12, y: 5 });
+    expect(() => movement.move(spaceObject)).toThrow('Velocity is not set');
+  });
+
+  it('throw exception if object is not moveble', () => {
+    const someObject = new UObject();
+    expect(() => movement.move(someObject)).toThrow('The object cannot move');
+  });
+
+  it('should rotate movable object', () => {
+    const spaceObject = new MovableObject();
+
+    spaceObject.setPosition({ x: 12, y: 5 });
+
+    rotation.rotate(spaceObject, 180);
+    expect(spaceObject.getAngle()).toBe(180);
+
+    rotation.rotate(spaceObject, 180);
+    expect(spaceObject.getAngle()).toBe(0);
+  });
+
+  it('throw exception if object is not rotatable', () => {
+    const someObject = new UObject();
+    expect(() => rotation.rotate(someObject, 180)).toThrow('The object cannot rotate');
+  });
+
+  it('throw exception if angle is not set', () => {
+    const spaceObject = new MovableObject();
+    expect(() => rotation.rotate(spaceObject)).toThrow('Angle is not set');
+  });
 });
