185a4fc4d5561d06e1828df1397c23ef7bac60bd
Irina Migunova <ista.irina@gmail.com>
2025-07-01 23:41:07 +0300

    solid-principles: solution to check if velocity and position set before move

--- solid-principles/solid-principles.js
+++ solid-principles/solid-principles.js
@@ -6,11 +6,12 @@ class MovableObject {
     this.y;
     this.velocity;
   }
-  
+
   setPosition({ x, y }) {
     this.x = x;
     this.y = y;
   }
+
   getPosition() {
     return { x: this.x, y: this.y };
   }
@@ -20,6 +21,14 @@ class MovableObject {
   }
 
   move() {
+    if (!this.x || !this.y) {
+      throw new Error('Position is not set');
+    }
+
+    if (!this.velocity) {
+      throw new Error('Velocity is not set');
+    }
+    
     this.x += this.velocity.x;
     this.y += this.velocity.y;
   }
--- solid-principles/solid-principles.test.js
+++ solid-principles/solid-principles.test.js
@@ -12,12 +12,12 @@ describe('SpaceObjects used SOLID principles', () => {
     it('throw exception if position is not set', () => {
         const spaceObject = new SpaceObject.MovableObject();
         spaceObject.setVelocity({ x: -7, y: 3 });
-        expect(spaceObject.move()).toThrow();
+        expect(() =>spaceObject.move()).toThrow();
     });
 
     it('throw exception if velocity is not set', () => {
         const spaceObject = new SpaceObject.MovableObject();
         spaceObject.setPosition({ x: 12, y: 5 });
-        expect(spaceObject.move()).toThrow();
+        expect(() => spaceObject.move()).toThrow();
     });
 });
