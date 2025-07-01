8dc93ca883076e463639e3909769a6d01cc9bcac
Irina Migunova <ista.irina@gmail.com>
2025-07-01 13:49:28 +0300

    module-tests: solution if one root

--- module-tests/quadratic-equation.js
+++ module-tests/quadratic-equation.js
@@ -9,9 +9,12 @@ class QuadraticEquation {
     const roots = [];
     const d = this.b * this.b - 4 * this.a * this.c;
 
-    if (d > 0) {
+    if (d >= 0) {
       roots.push((-this.b + Math.sqrt(d)) / (2 * this.a));
-      roots.push((-this.b - Math.sqrt(d)) / (2 * this.a));
+
+      if (d > 0) {
+        roots.push((-this.b - Math.sqrt(d)) / (2 * this.a));
+      }
     }
     
     return roots;
--- module-tests/quadratic.equation.test.js
+++ module-tests/quadratic.equation.test.js
@@ -9,3 +9,8 @@ test('should return an array length 2 for x^2 - 1 = 0', () => {
   const equation = new QuadraticEquation(1, 0, -1);
   expect(equation.solve()).toEqual([1, -1]);
 });
+
+test('should return an array length 1 for x^2 + 2x + 1 = 0', () => {
+  const equation = new QuadraticEquation(1, 2, 1);
+  expect(equation.solve()).toEqual([-1]);
+});
