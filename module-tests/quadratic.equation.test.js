1805bfb98aeb5420279d1bed8a844f6d215d83ed
Irina Migunova <ista.irina@gmail.com>
2025-07-01 13:18:51 +0300

    module-tests: test for 2 roots

--- module-tests/quadratic.equation.test.js
+++ module-tests/quadratic.equation.test.js
@@ -4,3 +4,8 @@ test('should return an empty array for x^2 + 1 = 0', () => {
   const equation = new QuadraticEquation(1, 0, 1);
   expect(equation.solve()).toEqual([]);
 });
+
+test('should return an array length 2 for x^2 - 1 = 0', () => {
+  const equation = new QuadraticEquation(1, 0, -1);
+  expect(equation.solve().toEqual([1, -1]));
+});
