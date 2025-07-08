99ac866b8bbf339f242b57a7155863faf89008f3
Irina Migunova <ista.irina@gmail.com>
2025-07-08 16:17:54 +0300

    multithreads: fix tests

--- multithreads/multithreads.test.js
+++ multithreads/multithreads.test.js
@@ -14,26 +14,36 @@ describe('Thread', () => {
     expect(thread.queueSize).toBe(3);
   });
 
-  test('hard stop immidiately stops thread', () => {
+  test('hard stop immidiately stops thread', (done) => {
     thread.start();
     thread.stop();
-    expect(thread.queueSize).toBe(2);
+    setImmediate(() => {
+      expect(thread.queueSize).toBeGreaterThan(0);
+      done();
+    });
   });
 
-  test('soft stop finish commands in queue', () => {
+  test('soft stop finish commands in queue', (done) => {
     thread.start();
     thread.softStop();
-    expect(thread.queueSize).toBe(0);
+
+    setImmediate(() => {
+      expect(thread.queueSize).toBe(0);
+      done();
+    });
   });
 
-  test('expect finish all commands if any throws error', () => {
+  test('expect finish all commands if any throws error', (done) => {
     thread.put(() => { throw new Error('Error'); });
-
     thread.put(() => jest.fn());
+
     thread.start();
     thread.softStop();
 
-    expect(thread.queueSize).toBe(0);
+    setImmediate(() => {
+      expect(thread.queueSize).toBe(0);
+      done();
+    });
   });
 
 });
